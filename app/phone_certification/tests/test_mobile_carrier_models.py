af4a88113124afc4a199a2f0b4014d389f973b3d
jhoon6886@gmail.com <pwhyun6246>
2022-08-27 23:55:06 +0900

    test: 통신사 모델 테스트 데이터 선언

--- app/phone_certification/tests/test_mobile_carrier_models.py
+++ app/phone_certification/tests/test_mobile_carrier_models.py
@@ -4,19 +4,21 @@
 from django.test import TestCase
 from phone_certification.models import MobileCarrier
 
+mock_mobile_carrier = {
+    "code": 1111,
+    "name": 'SK 텔레콤'
+}
+
 
 class MobileCarrierModelTests(TestCase):
     def test_default_mobile_carrier_values(self):
         """Test default values."""
-        mock_code = 1
-        mock_name = 'SK 텔레콤'
-
         mobile_carrier = MobileCarrier.objects.create(
-            code=mock_code,
-            name=mock_name
+            code=mock_mobile_carrier["code"],
+            name=mock_mobile_carrier["name"]
         )
 
-        self.assertEqual(mobile_carrier.code, mock_code)
-        self.assertEqual(mobile_carrier.name, mock_name)
+        self.assertEqual(mobile_carrier.code, mock_mobile_carrier["code"])
+        self.assertEqual(mobile_carrier.name, mock_mobile_carrier["name"])
 
 
